5206fce60597e1b1baae8e859041baba9d4ffc00
Alireza Andersen <alireza.andersen@gmail.com>
2016-06-15 23:52:21 -0600

    moved javascript to get functionality in heroku

--- turing/module3/week5/adventure_time/app/assets/javascripts/application.js
+++ turing/module3/week5/adventure_time/app/assets/javascripts/application.js
@@ -17,29 +17,3 @@
 //= require turbolinks
 //= require parks
 //= require underscore
-
-$(document).ready(function(){
-//var parks = document.querySelectorAll('.card-text');
-var zipcodes = _.uniq($('.card-text').map(function(){ return $(this).attr('data-zip') }))
-for (var i = 0; i < zipcodes.length; i++) {
-  //var zipcode = val.dataset['zip'];
-  var target_url = '/json/weather';
-  $.ajax({
-    url: target_url,
-    data: {
-      zip: zipcodes[i]
-    },
-    type: 'GET',
-    dataType: 'json'
-  }).done(function (json) {
-    //console.log(json['temp']);
-    var sel = "[data-zip=" + json['zip'] + "]";
-    $(sel).html(json['temp']);
-  }).fail(function (xhr, status, errorThrown) {
-    alert('Sorry, there was a problem!');
-    console.log('Error: ' + errorThrown);
-    console.log('Status: ' + status);
-    console.dir(xhr);
-  });
-}
-})
